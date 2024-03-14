87716452c6904ebd398d87c4d5f5fd670726f4d0
Li <1356683438@qq.com>
2024-03-14 15:55:56 +0800

    udpsocket

---
+++ udpsocket/server.c
@@ -0,0 +1,60 @@
+// server.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+
+
+
+int main()
+{
+    // 1. 创建监听的套接字
+    int lfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(lfd == -1)
+    {
+        perror("socket");
+        exit(0);
+    }
+
+    // 2. 将socket()返回值和本地的IP端口绑定到一起
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(10000);   // 大端端口
+    // INADDR_ANY代表本机的所有IP, 假设有三个网卡就有三个IP地址
+    // 这个宏可以代表任意一个IP地址
+    // 这个宏一般用于本地的绑定操作
+    addr.sin_addr.s_addr = INADDR_ANY;  // 这个宏的值为0 == 0.0.0.0
+//    inet_pton(AF_INET, "192.168.237.131", &addr.sin_addr.s_addr);
+    int ret = bind(lfd, (struct sockaddr*)&addr, sizeof(addr));
+    if(ret == -1)
+    {
+        perror("bind");
+        exit(0);
+    }
+
+char buf[1024];
+    char ipbuf[64];
+    struct sockaddr_in cliaddr;
+    int len = sizeof(cliaddr);
+    // 3. 通信
+    while(1)
+    {
+        // 接收数据
+        memset(buf, 0, sizeof(buf));
+        int rlen = recvfrom(lfd, buf, sizeof(buf), 0, (struct sockaddr*)&cliaddr, &len);
+        printf("客户端的IP地址: %s, 端口: %d\n", inet_ntop(AF_INET, &cliaddr.sin_addr.s_addr, ipbuf, sizeof(ipbuf)), ntohs(cliaddr.sin_port));
+        printf("客户端say: %s\n", buf);
+
+        // 回复数据
+        // 数据回复给了发送数据的客户端
+        sendto(lfd, buf, rlen, 0, (struct sockaddr*)&cliaddr, sizeof(cliaddr));
+    }
+
+
+    close(lfd);
+
+    return 0;
+}
+
